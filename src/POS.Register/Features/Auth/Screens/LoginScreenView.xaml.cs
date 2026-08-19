a39e9813b2449ff0bda9a3f2f682e0121f2380b9
Elmer Varquez <83869062+el-varquez@users.noreply.github.com>
2026-08-19 18:14:56 +0800

    feat: wire login flow to the api

--- src/POS.Register/Features/Auth/Screens/LoginScreenView.xaml.cs
+++ src/POS.Register/Features/Auth/Screens/LoginScreenView.xaml.cs
@@ -8,6 +8,50 @@ public partial class LoginScreenView : UserControl
     public LoginScreenView()
     {
         InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.OldValue is LoginScreenViewModel oldVm)
+        {
+            oldVm.RequestClearPasswords -= ClearPasswordBoxes;
+        }
+        if (e.NewValue is LoginScreenViewModel newVm)
+        {
+            newVm.RequestClearPasswords += ClearPasswordBoxes;
+        }
+    }
+
+    private void ClearPasswordBoxes()
+    {
+        PasswordBoxField.Clear();
+        NewPasswordBox.Clear();
+        ConfirmPasswordBox.Clear();
+    }
+
+    private void OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is LoginScreenViewModel vm)
+        {
+            vm.Password = PasswordBoxField.Password;
+        }
+    }
+
+    private void OnNewPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is LoginScreenViewModel vm)
+        {
+            vm.NewPassword = NewPasswordBox.Password;
+        }
+    }
+
+    private void OnConfirmPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is LoginScreenViewModel vm)
+        {
+            vm.ConfirmPassword = ConfirmPasswordBox.Password;
+        }
     }
 
     private void OnCloseClick(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
