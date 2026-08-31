0a5f3877247e38157a08f92657d28052c5a11060
Elmer Varquez <83869062+el-varquez@users.noreply.github.com>
2026-08-31 21:45:58 +0800

    feat!: one payment modal with method picker and invoice charge

--- src/POS.Register/Components/PaymentModalView.xaml.cs
+++ src/POS.Register/Components/PaymentModalView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace POS.Register.Components;
 
@@ -9,11 +10,24 @@ public partial class PaymentModalView : UserControl
         InitializeComponent();
         Loaded += (_, _) =>
         {
-            if (DataContext is PaymentModalViewModel { IsCash: true })
+            if (DataContext is not PaymentModalViewModel vm)
+            {
+                return;
+            }
+            if (vm.IsCash)
             {
                 TenderedBox.Focus();
                 TenderedBox.SelectAll();
             }
+            else if (vm.IsInvoice)
+            {
+                SearchBox.Focus();
+            }
         };
+    }
+
+    private void OnDownTextInput(object sender, TextCompositionEventArgs e)
+    {
+        e.Handled = !e.Text.All(char.IsAsciiDigit);
     }
 }
